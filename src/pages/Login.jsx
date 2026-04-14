e5b3a3b948d2591d2d0f9b18371f91f370ca6481
Emilykasande <kasandeemily67@gmail.com>
2026-04-14 16:11:02 +0300

    Fix login API URL to use server IP instead of localhost

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -16,7 +16,7 @@ export default function Login() {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:3001/auth/login", {
+      const response = await fetch("http://208.68.36.144/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
